688f5ae77880f0a0382cb29e8fd97b6cc4d6d397
khadyjh <khadyjh2015@gmail.com>
2022-03-19 11:59:05 +0300

    changed

--- challenge6/app/src/main/java/challenge6/App.java
+++ challenge6/app/src/main/java/challenge6/App.java
@@ -13,21 +13,27 @@ public class App {
 
 
 
-        LinkedList<Integer> linkedList=new LinkedList<>();
-        linkedList.insert(1);
-        linkedList.insert(2);
-        System.out.println(linkedList.includes(100));
-        linkedList.print();
-        linkedList.insert(55);
-        linkedList.append(12);
-        linkedList.append(11);
-        linkedList.InsertAfter(6,55);
-        linkedList.insertBefore(100,11);
-        linkedList.InsertAfter(100,11);
-        System.out.println(linkedList.toString());
+//        LinkedList<Integer> linkedList=new LinkedList<>();
+//        linkedList.insert(1);
+//        linkedList.insert(2);
+//        System.out.println(linkedList.includes(100));
+//        linkedList.print();
+//        linkedList.insert(55);
+//        linkedList.append(12);
+//        linkedList.append(11);
+//        linkedList.InsertAfter(6,55);
+//        linkedList.insertBefore(100,11);
+//        linkedList.InsertAfter(100,11);
+//        System.out.println(linkedList.toString());
 
         System.out.println("///////////////////////////////////////////////////////////////////////////////////////////");
 
         LinkedList<String> str=new LinkedList<>();
+        str.append("why");
+        str.append("are");
+        str.append("you");
+        str.append("here");
+        str.append("?");
+        System.out.println(str);
     }
 }
