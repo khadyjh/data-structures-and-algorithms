39083b82855cbaff337fb7c026769171e3064944
khadyjh <khadyjh2015@gmail.com>
2022-05-17 14:02:58 +0300

    commit

---
+++ challenge30/app/src/main/java/challenge30/binaryTree/BinaryNode.java
@@ -0,0 +1,37 @@
+package challenge30.binaryTree;
+
+public class BinaryNode<T> {
+    public T data;
+    public BinaryNode<T> left;
+    public BinaryNode<T> right;
+
+    public BinaryNode(T data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public BinaryNode<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(BinaryNode<T> left) {
+        this.left = left;
+    }
+
+    public BinaryNode<T> getRight() {
+        return right;
+    }
+
+    public void setRight(BinaryNode<T> right) {
+        this.right = right;
+    }
+}
